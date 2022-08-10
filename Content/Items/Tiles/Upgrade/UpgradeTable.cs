0e9b76ed96b1363b87816fd1b7e5393c765fd854
TeaFaris <95927550+TeaFaris@users.noreply.github.com>
2022-08-10 22:15:52 +0500

    Fixed your bug, Gleb...

--- Content/Items/Tiles/Upgrade/UpgradeTable.cs
+++ Content/Items/Tiles/Upgrade/UpgradeTable.cs
@@ -26,7 +26,6 @@ namespace JustEnoughScythes.Content.Items.Tiles.Upgrade
 			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
 			// Placement
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
-			TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
 			TileObjectData.newTile.CoordinatePaddingFix = new Point16(0, 2);
 			// The following 3 lines are needed if you decide to add more styles and stack them vertically
 			TileObjectData.newTile.StyleWrapLimit = 2;
